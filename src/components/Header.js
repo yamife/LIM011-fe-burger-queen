f4c31922a3711ea49be8974186bbea4b6e2c8d8c
yamiquispe <yamipqm@gmail.com>
2020-03-05 16:42:53 -0500

    Estilos de componente Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,17 +1,34 @@
 import React from 'react';
 
+
 class Header extends React.Component {
     render(){
         return(
             <header>
-                <nav class="navbar navbar-light bg-light">
-                <a class="navbar-brand" href="./">
-                    <img src="../public/img/logo.png" width="30" height="30" class="d-inline-block align-top" alt="" />Burger Queen
-                </a>
+                <nav className="navbar navbar-light bg-light">
+                    <a className="navbar-brand" href="./">
+                        <img src="logo.png" width="30" height="30" className="d-inline-block align-top" alt="" />Burger Queen
+                    </a>
+
+                    <ul class="nav nav-pills nav-end">
+                    <li className="nav-item">
+                        <a className="nav-link active" href="./">Inicio</a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link" href="./">Menú</a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link" href="./">Registro de Pedidos</a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link" href="./">Lista de pedidos</a>
+                    </li>
+                </ul>
                 </nav>
             </header>
         );
     }
 
 }
+
 export default Header;
