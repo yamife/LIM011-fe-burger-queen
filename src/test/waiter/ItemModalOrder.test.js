a72a6fed457dc4f1cbb5eed9846dd496f6bd8f24
yamiquispe <yamipqm@gmail.com>
2020-04-06 14:32:18 -0500

    Borrando comentarios de archivo ItemModalOrder.test.js

--- src/test/waiter/ItemModalOrder.test.js
+++ src/test/waiter/ItemModalOrder.test.js
@@ -1,63 +1,9 @@
 import React from 'react';
-// import { unmountComponentAtNode } from "react-dom";
-import { render, getNodeText } from '@testing-library/react';
-// import { act } from 'react-dom/test-utils';
-
+import { render } from '@testing-library/react';
 import ItemModalOrder from '../../components/waiter/order/ItemModalOrder';
 
-// let container = null;
-// beforeEach(() => {
-//   // configurar un elemento del DOM como objetivo del renderizado
-//   container = document.createElement("div");
-//   document.body.appendChild(container);
-// });
 
-// afterEach(() => {
-//   // limpieza al salir
-//   unmountComponentAtNode(container);
-//   container.remove();
-//   container = null;
-// });
-
-/* it('Deberia los datos de la tabla', () => {
-  const orderData = {
-    waiter: 'John Cena',
-    client: 'Yeni Diaz',
-    date: '1 de abril 2020',
-    order: [
-      {
-        id: '0112',
-        nameProduct: 'cafe',
-        price: 5,
-        quantity: 1,
-        subtotal: 5
-      }],
-    table: 1,
-    totalPay: 5,
-    state: false,
-  }
-
-  const order = render(
-    <div>
-      <table>
-      <thead>
-            <tr>
-              <th>Cantidad</th>
-              <th>Producto</th>
-              <th>Precio c/u</th>
-              <th>Sub Total</th>
-            </tr>
-          </thead>
-      <tbody>
-        <ItemModalOrder value={orderData}/>
-      </tbody>
-      </table>
-    </div>);
-
-const a = order.getByTestId('item1');
-// expect(getNodeText(listaDeNodos[0])).toBe(undefined);
-
-}); */
+afterEach(cleanup);
 
 
 const order = {
